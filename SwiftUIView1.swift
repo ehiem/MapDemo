3532d5ab2fba78aa2aa7a4d173abc280b5e0d468
E <elinorhiemstra@gmail.com>
2023-06-28 12:53:01 -0600

    Updated buttons on map and home

---
+++ SwiftUIView1.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView1.swift
+//  Map
+//
+//  Created by Scholar on 6/28/23.
+//AIBOT
+
+import SwiftUI
+
+struct SwiftUIView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView1_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView1()
+    }
+}
